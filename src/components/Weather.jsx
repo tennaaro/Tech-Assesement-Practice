50c71e1bbc3da068ca925412ba816144882e882e
Aaron Tenn <aaron.will.tenn@gmail.com>
2022-02-06 17:34:19 -0500

    got image pictures working

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 const Weather = (props) => {
   const {day, icon, max, min} = props
-
+  let iconurl = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
   return (
-    <p>{day} {icon} {max} {min}</p>
-
+    <>
+      <p>{day} {iconurl} {max} {min}</p>
+      <img src={iconurl} />
+    </>
   )
 }
 
